c8025832a6a9605782ad774ab178a016e98cfabe
CrashMaster8497 <crashmaster8497@gmail.com>
2022-09-14 02:15:26 +0800

    init: babel-jest

--- jest.config.js
+++ jest.config.js
@@ -181,7 +181,9 @@ module.exports = {
   // testRunner: "jest-circus/runner",
 
   // A map from regular expressions to paths to transformers
-  // transform: undefined,
+  transform: {
+    "^.+\\.[t|j]sx?$": "babel-jest"
+  },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
