570ec1ec2dc62acb672f16d9131f23d90c427848
CrashMaster8497 <crashmaster8497@gmail.com>
2022-09-19 04:35:56 +0800

    feat: enclosure

---
+++ src/Enclosure.js
@@ -0,0 +1,34 @@
+export default class Enclosure {
+    constructor(name, squareFeet) {
+        this.name = name || '';
+        this.squareFeet = squareFeet || 0;
+        this.animals = [];
+    }
+
+    addAnimal = (newAnimal) => {
+        let squareFeetLeft = this.squareFeet;
+        this.animals.forEach((animal) => {
+            squareFeetLeft -= animal.requiredSpaceSqFt;
+        });
+
+        if (squareFeetLeft < newAnimal.requiredSpaceSqFt) {
+            throw new Error(
+                `No available space in enclosure ${this.name}: ${newAnimal.requiredSpaceSqFt} required, ${squareFeetLeft} available`
+            );
+        }
+
+        const notFriendlyAnimals = this.animals.filter(
+            (animal) =>
+                !newAnimal.isFriendlyWith(animal) ||
+                !animal.isFriendlyWith(newAnimal)
+        );
+
+        if (notFriendlyAnimals.length > 0) {
+            throw new Error(
+                `Not friendly animal in enclosure ${this.name}: ${newAnimal} is not friendly with ${notFriendlyAnimals[0]}`
+            );
+        }
+
+        this.animals.push(newAnimal);
+    };
+}
